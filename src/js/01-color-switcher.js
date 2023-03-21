cacb9250e6f0020f2f31c7b179f926f993d2a2b4
DJlabuh <djlabuh@gmail.com>
2023-03-21 09:57:29 +0200

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,28 @@
+const startBtn = document.querySelector('[data-start]');
+const stopBtn = document.querySelector('[data-stop]');
 
+let intervalId = null;
+
+startBtn.addEventListener('click', onStartBtnClick);
+stopBtn.addEventListener('click', onStopBtnClick);
+
+function onStartBtnClick() {
+  startBtn.disabled = true;
+  stopBtn.disabled = false;
+
+  intervalId = setInterval(() => {
+    const color = getRandomHexColor();
+    document.body.style.backgroundColor = color;
+  }, 1000);
+}
+
+function onStopBtnClick() {
+  startBtn.disabled = false;
+  stopBtn.disabled = true;
+
+  clearInterval(intervalId);
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+}
