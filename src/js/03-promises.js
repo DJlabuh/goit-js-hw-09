16e750ac17f4d54b223a2e42d9548441f3c10570
DJlabuh <djlabuh@gmail.com>
2023-03-22 10:02:33 +0200

    Update 03-promises.js

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,42 @@
+import Notiflix from 'notiflix';
+
+const form = document.querySelector('.form');
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  });
+}
+
+function handleSubmit(event) {
+  event.preventDefault();
+
+  const delay = parseInt(form.elements.delay.value);
+  const step = parseInt(form.elements.step.value);
+  const amount = parseInt(form.elements.amount.value);
+
+  if (!delay || !step || !amount) {
+    Notiflix.Notify.failure('All fields are required!');
+    return;
+  }
+
+  for (let i = 1; i <= amount; i++) {
+    const promiseDelay = delay + (i - 1) * step;
+    createPromise(i, promiseDelay)
+      .then(({ position, delay }) => {
+        Notiflix.Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      })
+      .catch(({ position, delay }) => {
+        Notiflix.Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+      });
   }
 }
+
+form.addEventListener('submit', handleSubmit);
